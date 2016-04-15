30fc123521c73af48c1f1eb9794f28d04e46daab
PotapovMS <Potapov_MS@outlook.com>
2016-04-15 17:12:41 +0300

    Созданы новые классы, конструкторы и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,27 +1,20 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram {
-  public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Максим");
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-    double a = 4;
-    double b = 6;
-    System.out.println("Площаль прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    public class MyFirstProgram {
+      public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Максим");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площаль прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len) {
-    return len * len;
-  }
-  public static double area(double a, double b){
-    return a*b;
-  }
 }
 
 
