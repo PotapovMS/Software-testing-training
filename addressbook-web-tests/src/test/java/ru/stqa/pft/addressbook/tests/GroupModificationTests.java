ef4a84fb6078268c6078f0ea8867b290ef85a7f8
PotapovMS <Potapov_MS@outlook.com>
2016-05-09 15:05:02 +0300

    В тестах для групп добавлено сравнение количества групп до и после тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -10,15 +11,18 @@ public class GroupModificationTests extends TestBase {
 
   @Test
   public void testModificationGroup() {
-  app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()){
-      app.getGroupHelper().createGroup (new GroupData("test1", "test2", null));
-  app.getGroupHelper().selectGroup();
-  app.getGroupHelper().editGroup();
-  app.getGroupHelper().fillGroupForm(new GroupData("a", "b", "c"));
-  app.getGroupHelper().submitGroupModification();
-  app.getGroupHelper().returnToGroupPage();
+    app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", null));
+      app.getGroupHelper().selectGroup();
+      app.getGroupHelper().editGroup();
+      app.getGroupHelper().fillGroupForm(new GroupData("a", "b", "c"));
+      app.getGroupHelper().submitGroupModification();
+      app.getGroupHelper().returnToGroupPage();
     }
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
   }
 }
 
