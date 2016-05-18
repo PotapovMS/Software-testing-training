5740f9b5cbc574d1965efb71378fafe5db12a79b
PotapovMS <Potapov_MS@outlook.com>
2016-05-19 00:25:10 +0300

    реализован тест для сверки адреса контакта главной страницы и страницы редактирования

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Максим on 19.05.2016.
+ */
+public class ContactAddressTests extends TestBase {
+
+  @Test
+  public void testContactAddress(){
+    app.goTo().goToHomePage();
+    if (!app.contact().isThereAContact()) {
+      app.contact().createContact(new ContactData().withFirstname("FirstName").withLastname("LastName").withGroup("a").withAddress("Address1"));
+    }
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+  }
+}
+
